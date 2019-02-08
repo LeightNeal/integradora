028028dc35ea6ee4fa06749a2c050362bf8cf38a
salvador941130 <salvador.martinez@alumno.uttehuacan.edu.mx>
2019-02-08 08:26:14 -0600

    ranking

--- src/pages/ranking/ranking.ts
+++ src/pages/ranking/ranking.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RankingPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import chartJs from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -14,6 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ranking.html',
 })
 export class RankingPage {
+  
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('doughnutCanvas2') doughnutCanvas2;
+
+  doughnutChart : any;
+  doughnutChart2 : any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -22,4 +22,50 @@ export class RankingPage {
     console.log('ionViewDidLoad RankingPage');
   }
 
+  ngAfterViewInit(){  
+    setTimeout(() => {      
+      this.doughnutChart = this.getDoughnutChart();
+      this.doughnutChart2 = this.getDoughnutChart2();
+    }, 250)
+  }
+
+  getChart(context, chartType, data, options?) {
+    return new chartJs(context, {
+      data,
+      options,
+      type: chartType
+    })
+  }
+
+  getDoughnutChart(){
+    const data = {
+      labels: ['Retos', 'Libre', 'Memoria'],
+      datasets: [{
+        label: 'Teste Chart',
+        data: [10, 50, 32],
+        backgroundColor: [
+          'rgb(42, 123, 227)',
+          'rgb(242, 80, 44)',
+          'rgb(255, 207, 0)'
+        ]
+      }]
+    }
+    return this.getChart(this.doughnutCanvas.nativeElement, 'doughnut', data);
+  }
+  
+  getDoughnutChart2(){
+    const data = {
+      labels: ['Retos', 'Libre', 'Memoria'],
+      datasets: [{
+        label: 'Teste Chart',
+        data: [80, 20, 50],
+        backgroundColor: [
+          'rgb(42, 123, 227)',
+          'rgb(242, 80, 44)',
+          'rgb(255, 207, 0)'
+        ]
+      }]
+    }
+    return this.getChart(this.doughnutCanvas2.nativeElement, 'doughnut', data);
+  }
 }
